400e23848ad56cce94a777dede9a8483fce29c0f
colletemine <jepngetichcolleta@gmail.com>
2022-03-05 14:24:46 +0300

    ft:head-add bootstrap link

---
+++ js/index.js
@@ -0,0 +1,28 @@
+//names for male and females
+var maleName = ["Kwasi", "Kwa", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+// find the date
+function findDate() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
