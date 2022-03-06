1f75be4afeddccacb5eb6e4b84513b3ce4ab0a41
colletemine <jepngetichcolleta@gmail.com>
2022-03-06 17:00:36 +0300

    ft:js-add day validation

--- js/index.js
+++ js/index.js
@@ -1,51 +1,16 @@
-//creating arrays for male and females names,and days of the week
-let maleName = ["Kwasi", "Kwa", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-let femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-let weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+let year = document.querySelector("#year").value;
+let month = document.querySelector("#month").value;
+let day = document.querySelector("#day").value;
 
-
-// find the date
-function findDate() {
-let year = document.getElementById("year").value;
-let mm = document.getElementById("month").valu;
-let dd = document.getElementById("day").value;
-let gender = document.getElementById("gender");
-// let cc = year.substring(0,2);
-// let yy = year.substring(2,4);
-let akanName = Math.floor((((Number(year.slice(0,2))cc/4) -2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd ) %7);
-// validate the form
-if (dd <= 0 || dd=="" || dd>31) {
-  alert("invalid date")
-
-} else if (mm <= 0 || mm=="" || mm>12) {
-  alert("invalid month");
-} 
-if (gender === "male"){
-  alert("Your Akan name is " + maleName[akanName]);
-} else if (gender === "female"){
-  alert("your Akan name is" + femaleName[akanName]);
-  console.log(dd,mm,gender,year);
-
+// validation of the day
+const message = document.getElementById("errDay");
+console.log(message)
+message.innerHTML = "";
+let day = document.getElementById("day").value;
+try {
+  if(day== "") throw "empty";
+  day = Number(day);
+  if(day > 31) throw "Invalid date";
+  if(day <= 0) throw "Invalid date";
+  if(day >29 && month=== 2) throw "Invalid date"
 }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
